ba9e6b8a064ea63563bcadbdf9fcd4637ab974f6
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-09 01:16:34 -0300

    created feedback POST route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import express from 'express'
+import { prisma } from './prisma';
+
 
 const app = express();
 const port = 3333;
 
-app.get('/users', (req,res) => {
-    return( res.send('Alô, moleque!'))
+//GET,POST,PUT,PATCH,DELETE
+app.use(express.json())
+
+app.post('/feedbacks', async (req,res) => {
+    const {type,comment,screenshot}= req.body
+    
+    const feedback = await prisma.feedback.create({
+        data:{
+            type,
+            comment,
+            screenshot
+        }
+    })
+
+    return( res.status(201).json({data:feedback}))
+    
 })
 
 app.listen(port, () => {
