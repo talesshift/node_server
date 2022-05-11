0b70b74d23a6701e45d6906aa579789a34d63ffc
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-10 23:52:50 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,17 @@ import { mailtrap_auth, my_email } from './data';
 import express from 'express'
 import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
+import cors from 'cors'
 
 
 const app = express();
 const port = 3333;
 
 //GET,POST,PUT,PATCH,DELETE
+app.use(cors(
+    
+));
 app.use(express.json())
-
 app.use(routes)
 
 app.listen(port, () => {
